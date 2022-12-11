3b7c55a9d4378f3d9dbd4b1545c4c07dac133fec
yrevna <yr.evna@mail.ru>
2022-12-12 02:22:55 +0300

    finish task_5_js

---
+++ js_course/task_5_2/script.js
@@ -0,0 +1,5 @@
+function square() {
+    let a = document.getElementById('dlina');
+    let b = document.getElementById('shirina');
+    alert(`Площадь прямоугольника: ${parseInt(a.value)*parseInt(b.value)}`);
+}
